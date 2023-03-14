9aa1592ee8b1b1e80b3f410ec263c47494a56c69
Zenzele Hlubi <2467513@students.wits.ac.za>
2023-03-14 18:11:46 +0200

    first upload

---
+++ Wits_Academy/app/src/main/java/com/example/wits_academy/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.wits_academy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        RelativeLayout relativeLayout = findViewById(R.id.login_page);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+
+    public void Main_page(View view) {
+        setContentView(R.layout.main_menu_students);
+    }
+}
