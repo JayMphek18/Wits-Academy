2d3adeacb4102090de9a4cce679da3982307eea7
Real-hash <127694782+Real-hash@users.noreply.github.com>
2023-04-23 20:37:54 +0200

    Update enroll.php

--- Wits_academy_web_files/student/enroll.php
+++ Wits_academy_web_files/student/enroll.php
@@ -15,6 +15,126 @@
     <!--<link type="text/css" href="create_course.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<style>
+html {
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    font-family: sans-serif;
+  }
+  
+ html {
+    height: 100%;
+
+  }
+   .field-label-3 {
+    width: 50%;
+    color: #c4d1db;
+    font-family: Droid Sans, sans-serif;
+    font-size: 30px;
+    position: relative;
+    top: -40px;
+    left: 264px;
+  }
+  body {
+    min-height: 100%;
+    
+  }
+  
+  img {
+    max-width: 100%;
+    vertical-align: middle;
+    display: inline-block;
+  }
+  
+  
+  
+
+  
+  .w-button {
+    color: #fff;
+    line-height: inherit;
+    cursor: pointer;
+    background-color: #3898ec;
+    border: 0;
+    border-radius: 0;
+    padding: 9px 15px;
+    text-decoration: none;
+    display: inline-block;
+  }
+  
+  html[data-w-dynpage] [data-w-cloak] {
+    color: rgba(0, 0, 0, 0) !important;
+  }
+  
+  
+  h1, h2, h3, h4, h5, h6 {
+    color: #c4d1db;
+    font-family: Droid Sans;
+    margin-bottom: -2px;
+    padding-left: 250px;
+    padding-right: 10px;
+    font-weight: bold;
+    position: relative
+    top:-40px;
+    
+  }
+ 
+   label {
+    margin-bottom: 5px;
+    font-weight: bold;
+    display: block;
+  }
+  
+   .image {
+    background-color: #1a2852;
+  }
+  
+  .section {
+    width: 1334px;
+    background-color: #1a2852;
+  }
+  
+  .body {
+    background-color: #bed2e2;
+    border: 1px solid #fff;
+  }
+  
+  .select-field {
+    width: 80%;
+    opacity: 1;
+    text-align: left;
+    object-fit: scale-down;
+    object-position: 50% 50%;
+    border: 1px solid #000;
+    border-radius: 20px;
+    font-size: 14px;
+    position: relative;
+    top: 145px;
+    bottom: -175px;
+    left: 146px;
+    right: -13px;
+  }
+  
+  .submit-button {
+    background-color: #c1c1c4e;
+    border-radius: 18px;
+    position: relative;
+    top: 154px;
+    left: 639px;
+  }
+  
+  .button {
+    color: #c4d1db;
+    background-color: #1a2852;
+    font-family: Droid Sans, sans-serif;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 700;
+    position: relative;
+    top: -75px;
+    left: 1015px;
+  }
+</style>
 </head>
 <body class="body">
     <div class="section">
