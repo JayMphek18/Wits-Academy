219558e60e97b2dc2656668f04b49919bf1056b4
JayMphek18 <127724323+JayMphek18@users.noreply.github.com>
2023-05-18 01:04:09 +0200

    Add files via upload
    The layout of the participants page is kept the same for now. New columns has been added to the table where the user can view the participants profile picture, as well as a link to allow them to send an email to their email address.

--- Wits_academy_web_files/teacher/participants.php
+++ Wits_academy_web_files/teacher/participants.php
@@ -16,29 +16,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
     <body class="body">
-    <div class="section">
+   
+    <?php
+        $id = $_GET['id'];
+        ?>
+         <div class="section">
         <img src="./Wits_Logo.png" loading="lazy" width="239" alt="Wits emblem" />
         <h1 class="head1">Participants</h1>
-       <a href="./index.php" class="link"><i class="fas fa-gauge"></i> Back To Dashboard</a>
-    </div>
-    <label>
- 
-    </label>
-    <?php
-                     $code = $_GET['id'];
-        $sql="select * from courses where course_code='$code'";
+       <a href="./course.php?id=<?php echo $id ?>" class="link"><i class="fas fa-house"></i> Back To Home</a>
+        </div>
+        <?php 
+        $sql="select * from enrollment where course_id='$id'";
         $result=$conn->query($sql);
         $row=$result->fetch_assoc();
-
-                    $user_id=$_SESSION['user_id'];
-                    $sql="select * from enrollment where student_num='$user_id'";
-                    $result=$conn->query($sql);
-                    $rowcount=mysqli_num_rows($result);
-                    if(!($rowcount))
-                        echo "<br><center><h2><b><i>No participants in this course</i></b></h2></center>";
-                    else
-                    {
-                    ?>
+        $rowcount=mysqli_num_rows($result);
+        if(!($rowcount))
+            echo "<br><center><h2><b><i>No participants in this course</i></b></h2></center>";
+        else
+        {
+        ?>
                     <table class="table" id = "tables">
                     <thead>
                     <tr>
@@ -46,31 +42,28 @@
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Student Number</th>
+                        <th>Email Address</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php
-          
+    <?php
             //$result=$conn->query($sql);
-        $sql1="select * from enrollment inner join registration where enrollment.student_num=registration.user_id && course_code='$code'";
- $result1=$conn->query($sql1);
+        $sql1="select * from enrollment inner join registration where enrollment.student_num=registration.user_id && course_id='$id'";
+        $result1=$conn->query($sql1);
         while($row=$result1->fetch_assoc())
-            {
-               
+            { 
               ?>
                     <tr>
-                        <td>
-                    
-                        
-                        <td><?id=<?php echo $code ?>"><?php echo $row['first_name'] ?></a></td>
-                        <td><?id=<?php echo $code ?>"><?php echo $row['last_name'] ?></a></td>
-                        <td><?id=<?php echo $code ?>"><?php echo $row['student_num'] ?></a></td>
-                    
-                        
+                        <td><img src="../profile_pic/<?php echo $row['profile_pic']?>" alt="profile_picture" loading="lazy" width="50"></td>
+                        <td><?php echo $row['first_name'] ?></td>
+                        <td><?php echo $row['last_name'] ?></td>
+                        <td><?php echo $row['student_num'] ?></td> 
+                        <td><a href="mailto: <?php echo $row['email_address'] ?>"><?php echo $row['email_address'] ?></a></td>  
                     </tr>
                 <?php }} ?>
                 </tbody>
-                </table>
+            </table>
+        
     </body>
 
 </html>
