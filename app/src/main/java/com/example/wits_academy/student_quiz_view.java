ff5fc94c2197c811a6424bc4723fe84823471479
zsmhlubi <126089780+zsmhlubi@users.noreply.github.com>
2023-06-03 09:57:24 +0200

    Update student_quiz_view.java

--- app/src/main/java/com/example/wits_academy/student_quiz_view.java
+++ app/src/main/java/com/example/wits_academy/student_quiz_view.java
@@ -121,7 +121,7 @@ public class student_quiz_view extends AppCompatActivity implements NavigationVi
                 intent.putExtra("Role","Student");
                 startActivity(intent);
             case R.id.courseHomeActivity:
-                Intent intent5 = new Intent(this,courseHomeActivity.class);
+                Intent intent5 = new Intent(this,student_course_view.class);
                 intent5.putExtra("courseName",courseName);
                 intent5.putExtra("userNumber",userNumber);
                 startActivity(intent5);
